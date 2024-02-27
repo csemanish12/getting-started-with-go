371dd41a019b0e020fdcf549c75255d601fe337f
Manish Sah <sahmanish20@gmail.com>
2024-02-27 23:22:22 +0545

    handle error

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -1,12 +1,20 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+
+)
 
 // hello returns greetings for named parameter
 // function name starting with Capital letter can be called from function which are not in same package
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// returns error if name is empty
+	if name == "" {
+		return "", errors.New("Empty name")
+	}
 	// returns a greetings that embeds name in the message
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	// := declare and initialize variable in one line
-	return message
+	return message, nil
 }
