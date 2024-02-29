4242d34eeb14005fd565c02b2fd0e7f385bf2ecc
Manish Sah <sahmanish20@gmail.com>
2024-02-29 20:59:37 +0545

    create constraint using struct

--- generics/main.go
+++ generics/main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int64 | float64
+}
+
 func main(){
 	// initialize map for integer values
 	ints := map[string]int64 {
@@ -20,6 +24,14 @@ func main(){
 	fmt.Printf("Generic Sums: %v and %v\n",
 SumIntsOrFloats[string, int64](ints),
 SumIntsOrFloats[string, float64](floats))
+
+fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+    SumIntsOrFloats(ints),
+    SumIntsOrFloats(floats))
+
+fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+    SumNumbers(ints),
+    SumNumbers(floats))
 
 }
 
@@ -43,6 +55,15 @@ func SumFloats(m map[string]float64) float64{
 
 func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
 	var sum V
+	for _, value := range m {
+		sum += value
+	}
+	return sum
+}
+
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+	var sum V
+
 	for _, value := range m {
 		sum += value
 	}
